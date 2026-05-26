e4f1810b9f6335bff5ae9223de567980e07e3755
Y-O-W <yves@yowfactor.com>
2026-05-26 18:04:27 +0200

    created new and create for content reports model, created 10 seeds 20260526

--- app/controllers/content_reports_controller.rb
+++ app/controllers/content_reports_controller.rb
@@ -6,14 +6,27 @@ class ContentReportsController < ApplicationController
   end
 
   def new
+    @content_report = ContentReport.new
   end
 
   def create
+    @content_report = ContentReport.new(content_report_params)
+    if @content_report.save
+      redirect_to @content_report, notice: "Content report was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def content_report_params
+    params.require(:content_report).permit(:content_type, :content_hook, :content_lenght, :content_platform, :system_prompt)
   end
 end
