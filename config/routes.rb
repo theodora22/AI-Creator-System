1374a772779add4d3f4bb426a3ca5ff2741a4a0c
Y-O-W <yves@yowfactor.com>
2026-05-27 16:20:35 +0200

    create new chat 20260527

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :content_reports, path: "content-reports", only: [
-    :index, :show, :new, :create, :edit, :update
-  ] do
-    resources :chats, only: [:create]
+  resources :content_reports, only: [ :index, :show, :new, :create, :edit, :update ] do
+    resources :chats, only: [ :create ]
   end
 
-  resources :chats, only: [:show] do
-    resources :messages, only: [:create]
+  resources :chats, only: [ :show ] do
+    resources :messages, only: [ :create ]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
