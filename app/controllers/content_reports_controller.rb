551161e7d1b6fa5656ca795c71486a7ba288813d
Armand Abbas <armandabbas@web.de>
2026-05-28 15:14:43 +0200

    feat: save AI response from strategy chat to new Content Report

--- app/controllers/content_reports_controller.rb
+++ app/controllers/content_reports_controller.rb
@@ -1,15 +1,16 @@
 class ContentReportsController < ApplicationController
+  before_action :set_content_report, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @content_reports = ContentReport.order(created_at: :desc)
   end
 
   def show
-    @content_report = ContentReport.find(params[:id])
     @chats = @content_report.chats.where(user: current_user).order(created_at: :desc)
   end
 
   def new
-    @content_report = ContentReport.new
+    @content_report = ContentReport.new(new_content_report_params)
   end
 
   def create
@@ -24,27 +25,30 @@ class ContentReportsController < ApplicationController
   def edit
   end
 
+  def update
+    if @content_report.update(content_report_params)
+      redirect_to @content_report, notice: "Content report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
- def update
-      if @content_report.update(content_report_params)
-        redirect_to @content_report, notice: "Content report was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
- end
-
- def destroy
-      @content_report.destroy
-      redirect_to content_reports_path, notice: "Content report was deleted."
- end
+  def destroy
+    @content_report.destroy
+    redirect_to content_reports_path, notice: "Content report was deleted."
+  end
 
   private
 
   def set_content_report
-        @content_report = ContentReport.find(params[:id])
+    @content_report = ContentReport.find(params[:id])
   end
 
   def content_report_params
     params.require(:content_report).permit(:content_title, :content_type, :content_hook, :content_length, :content_platform, :system_prompt)
+  end
+
+  def new_content_report_params
+    params.permit(:content_title, :content_type, :content_hook, :content_length, :content_platform, :system_prompt)
   end
 end
